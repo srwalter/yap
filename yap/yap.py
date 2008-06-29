3cbedf3bea65a913e5c59632659a222f163f004c
Steven Walter <stevenrwalter@gmail.com>
2008-06-29 12:31:31 -0400

    Implement argument parsing and command delegation
    The main() function processes argv and calls one of yap's subcommand methods.

--- yap/yap.py
+++ yap/yap.py
@@ -1,4 +1,50 @@
+import sys
+import getopt
+
+class YapError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+def takes_options(options):
+    def decorator(func):
+        func.options = options
+        return func
+    return decorator
 
 class Yap(object):
-    def main(self, *args):
-        print str(args)
+    def cmd_version(self):
+        print "Yap version 0.1"
+
+    def cmd_usage(self):
+        print >> sys.stderr, "usage: %s <command>" % sys.argv[0]
+        print >> sys.stderr, "  valid commands: version"
+
+    def main(self, args):
+        if len(args) < 1:
+            self.cmd_usage()
+            sys.exit(2)
+
+        command = args[0]
+        args = args[1:]
+
+        try:
+            meth = self.__getattribute__("cmd_"+command)
+            try:
+                if "option" in meth.__dict__:
+                    flags, args = getopt.getopt(args, meth.options)
+                    flags = dict(flags)
+                else:
+                    flags = dict()
+
+                meth(*args, **flags)
+            except (TypeError, getopt.GetoptError):
+                print "%s %s %s" % (sys.argv[0], command, meth.__doc__)
+            except YapError, e:
+                print >> sys.stderr, e
+                sys.exit(1)
+        except AttributeError:
+            self.cmd_usage()
+            sys.exit(2)
