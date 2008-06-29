4d51a41aa7e7fcf3ebc6ae1a46ac0d177c04bf04
Steven Walter <stevenrwalter@gmail.com>
2008-06-29 12:48:20 -0400

    Implement clone, init, add

--- yap/yap.py
+++ yap/yap.py
@@ -1,5 +1,14 @@
 import sys
+import os
 import getopt
+
+def get_output(cmd):
+    fd = os.popen(cmd)
+    output = fd.readlines()
+    rc = fd.close()
+    if len(output) == 1:
+        return output[0]
+    return output
 
 class YapError(Exception):
     def __init__(self, msg):
@@ -15,6 +24,21 @@ def takes_options(options):
     return decorator
 
 class Yap(object):
+    def cmd_clone(self, url, directory=""):
+        # XXX: implement in terms of init + remote add + fetch
+        os.system("git clone '%s' %s" % (url, directory))
+
+    def cmd_init(self):
+        os.system("git init")
+
+    def cmd_add(self, file):
+        if not os.access(file, os.R_OK):
+            raise YapError("No such file: %s" % file)
+        x = get_output("git ls-files '%s'" % file)
+        if x != []:
+            raise YapError("File '%s' already in repository" % file)
+        os.system("git update-index --add '%s'" % file)
+
     def cmd_version(self):
         print "Yap version 0.1"
 
@@ -30,6 +54,8 @@ class Yap(object):
         command = args[0]
         args = args[1:]
 
+        debug = os.getenv('YAP_DEBUG')
+
         try:
             meth = self.__getattribute__("cmd_"+command)
             try:
@@ -40,11 +66,15 @@ class Yap(object):
                     flags = dict()
 
                 meth(*args, **flags)
-            except (TypeError, getopt.GetoptError):
+            except (TypeError, getopt.GetoptError), e:
+                if debug:
+                    raise e
                 print "%s %s %s" % (sys.argv[0], command, meth.__doc__)
             except YapError, e:
                 print >> sys.stderr, e
                 sys.exit(1)
-        except AttributeError:
+        except AttributeError, e:
+            if debug:
+                raise e
             self.cmd_usage()
             sys.exit(2)
