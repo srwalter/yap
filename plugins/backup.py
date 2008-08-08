dc4cb2e9db9b587be273313d0ad7196a159ea984
Steven Walter <swalter@lpdev.prtdev.lexmark.com>
2008-08-08 09:10:36 -0400

    backup: be quiet if the file doesn't exist
    This could happen during clone, for example

--- plugins/backup.py
+++ plugins/backup.py
@@ -1,5 +1,6 @@
 
 from yap.yap import YapCore, YapError
+from yap.util import run_command
 import os
 
 class BackupPlugin(YapCore):
@@ -15,5 +16,5 @@ class BackupPlugin(YapCore):
             x = files.intersection(changed)
 
         for f in x:
-            os.system("cp %s %s~" % (f, f))
+            run_command("cp %s %s~" % (f, f))
 	super(BackupPlugin, self).cmd_revert(*args, **flags)
