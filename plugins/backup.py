b52e49a4b3b324f61085a80e7e2b0717a0cc3a86
Steven Walter <swalter@lpdev.prtdev.lexmark.com>
2008-10-15 16:28:36 -0400

    backup: it only makes sense to backup files that exist

--- plugins/backup.py
+++ plugins/backup.py
@@ -11,10 +11,12 @@ class BackupPlugin(YapCore):
         changed = set(self._get_staged_files() + self._get_unstaged_files())
 
         if '-a' in flags:
-            x = changed
+            files = changed
         else:
-            x = files.intersection(changed)
+            files = files.intersection(changed)
 
-        for f in x:
-            run_command("cp %s %s~" % (f, f))
+	files = [ x for x in files if os.access(x, os.R_OK) ]
+
+        for f in files:
+	    run_command("cp %s %s~" % (f, f))
 	super(BackupPlugin, self).cmd_revert(*args, **flags)
